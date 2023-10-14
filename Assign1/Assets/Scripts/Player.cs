e5137e79a7061b3efad550f781cde5f29adff7c7
Joshua Hardwick <200560630@student.georgianc.on.ca>
2023-10-14 19:06:48 -0400

    Added Explosion Collision with Boxes

--- Assign1/Assets/Scripts/Player.cs
+++ Assign1/Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Player : MonoBehaviour
 {
@@ -11,6 +12,12 @@ public class Player : MonoBehaviour
     public int bombCount = 1;
     private int bombsRemaining;
 
+    public Explosion explosionPrefab;
+    public float explosionLength = 2.0f;
+    public int explosionRadius = 1;
+    public LayerMask explosionLayerMask;
+
+    public Tilemap destroyableTiles;
 
     void Start()
     {
@@ -72,7 +79,57 @@ public class Player : MonoBehaviour
         Debug.Log("Bomb Placed");
 
         yield return new WaitForSeconds(explosionTime);
+
+        position = bomb.transform.position;
+
+        Explosion explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
+        explosion.SetActiveRenderer(explosion.Start);
+        explosion.DestroyAfter(explosionTime);
+
+        Explode(position, Vector2.up, explosionRadius);
+        Explode(position, Vector2.down, explosionRadius);
+        Explode(position, Vector2.left, explosionRadius);
+        Explode(position, Vector2.right, explosionRadius);
+
+
+
+
         Destroy(bomb);
         bombsRemaining++;
+    }
+
+    private void Explode(Vector2 position, Vector2 direction, int length)
+    {
+        if (length <= 0)
+        {
+            return;
+        }
+
+        position += direction;
+
+        if (Physics2D.OverlapBox(position, Vector2.one / 2f, 0f, explosionLayerMask))
+        {
+            ClearDestroyable(position);
+            return;
+        }
+
+        Explosion explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
+        explosion.SetActiveRenderer(length > 1 ? explosion.Middle : explosion.End);
+        explosion.SetDirection(direction);
+        explosion.DestroyAfter(explosionTime);
+
+
+        Explode(position, direction, length - 1);
+    }
+
+    private void ClearDestroyable(Vector2 position)
+    {
+        Vector3Int cell = destroyableTiles.WorldToCell(position);
+        TileBase tile = destroyableTiles.GetTile(cell);
+
+        if (tile != null)
+        {
+            destroyableTiles.SetTile(cell, null);
+        }
     }
 }
